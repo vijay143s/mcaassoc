4a44239b4f2a2ea2996a51a2c764625b732c4999
vijay <vijay.ramireddy45@gmail.com>
2026-02-13 12:36:53 +0530

    Fix Redux DevTools usage with manual compose

--- src/redux/customstore.js
+++ src/redux/customstore.js
@@ -1,12 +1,8 @@
-import { applyMiddleware } from "redux";
-import { createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore, compose } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhancers = composeWithDevTools({
-    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-});
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const customstore = createStore(
     rootReducer, composeEnhancers(applyMiddleware(thunk))
